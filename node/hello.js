dfcc1b2dad967a338921e54ab38d3a0b27864abd
thousandsofthem <thousandsofthem@gmail.com>
2015-03-12 01:04:51 +0300

    updated node example

--- node/hello.js
+++ node/hello.js
@@ -1,10 +1,13 @@
 
-var iw = require('node_helper');
-var iron_mq = require('iron_mq');
+var iron_worker = require('iron_worker');
 
-console.log("task_id:", iw.task_id);
-console.log("params:", iw.params);
-console.log("config:", iw.config);
+// Get task runtime data
+console.log("task_id:", iron_worker.taskId());
+console.log("params:", iron_worker.params());
+console.log("config:", iron_worker.config());
+
+// IronMQ usage example
+var iron_mq = require('iron_mq');
 
 var imq = new iron_mq.Client({token: "MY_TOKEN", project_id: "MY_PROJECT_ID", queue_name: "MY_QUEUE"});
 
