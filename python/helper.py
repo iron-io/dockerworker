6a3431aac12169881f58ca1e449e2b3f86779feb
Tague Griffith <tgriffith@gopro.com>
2015-08-24 22:21:53 -0700

    Update processing of worker task parameters.
    Follow pattern similar to the Ruby example code and fetch initially from the os environment, then from command line parameters.

--- python/helper.py
+++ python/helper.py
@@ -1,12 +1,16 @@
-import sys, json
+import sys, json, os
 
 
 def getPayload():
     file = None
-    for i in range(len(sys.argv)):
-        if sys.argv[i] == '-payload' and (i + 1) < len(sys.argv):
-            file = sys.argv[i + 1]
-            break
+    
+    if os.environ.has_key('PAYLOAD_FILE'):
+        file = os.environ['PAYLOAD_FILE']
+    else:
+        for i in range(len(sys.argv)):
+            if sys.argv[i] == '-payload' and (i + 1) < len(sys.argv):
+                file = sys.argv[i + 1]
+                break
 
     with open(file) as content:
         data = json.load(content)
@@ -16,9 +20,13 @@ def getPayload():
 
 def getTaskId():
     task_id = None
-    for i in range(len(sys.argv)):
-        if sys.argv[i] == '-id' and (i + 1) < len(sys.argv):
-            task_id = sys.argv[i + 1]
-            break
+    
+    if os.environ.has_key('TASK_ID'):
+        task_id = os.environ['TASK_ID']
+    else:
+        for i in range(len(sys.argv)):
+            if sys.argv[i] == '-id' and (i + 1) < len(sys.argv):
+                task_id = sys.argv[i + 1]
+                break
 
     return task_id
