057d8fa3513cad07e9e9a3e4022191ea5a96dbff
Travis Reeder <treeder@gmail.com>
2015-12-28 17:25:45 -0800

    Vendors first.

--- run_all.sh
+++ run_all.sh
@@ -6,26 +6,31 @@ dj() {
 
 echo "ruby"
 cd ruby
+dj ruby vendor
 dj -e "PAYLOAD_FILE=hello.payload.json" -e "YOUR_ENV_VAR=ANYTHING" ruby run hello.rb
 cd ..
 
 echo "go"
 cd go
+dj go vendor
 dj go build
 dj -e "PAYLOAD_FILE=hello.payload.json" -e "YOUR_ENV_VAR=ANYTHING" go run
 cd ..
 
 echo "node"
 cd node
+dj node vendor
 dj -e "PAYLOAD_FILE=hello.payload.json" -e "YOUR_ENV_VAR=ANYTHING" node run hello.js
 cd ..
 
 echo "php"
 cd php
+dj php vendor
 dj -e "PAYLOAD_FILE=hello.payload.json" -e "YOUR_ENV_VAR=ANYTHING" php run hello.php
 cd ..
 
 echo "python"
 cd python
+dj python vendor
 dj -e "PAYLOAD_FILE=hello.payload.json" -e "YOUR_ENV_VAR=ANYTHING" python run hello.py
 cd ..
